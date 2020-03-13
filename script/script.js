28ed84c8c47da4d736d4284475865776bd059bc6
TexMan123 <61990587+TexMan123@users.noreply.github.com>
2020-03-13 21:55:26 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,15 @@
+function verif() {
+	if(document.formMot.nom.value==""||document.formMot.tel.value==""||
+		document.formMot.email.value==""||document.formMot.sujet.value==""||
+		document.formMot.textarea.value==""){
+			alert('Merci de remplir tous les champs!');
+	}
+	else {
+		alert('ok');
+		
+
+	}	
+}
+
+
+
