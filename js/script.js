9dcb471e2cadd732011fe99562081bd754f68a5e
TexMan123 <61990587+TexMan123@users.noreply.github.com>
2020-03-16 18:20:36 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+ $(document).ready(function() {
+    $('#pagepiling').pagepiling({
+    	direction: 'horizontal',
+        anchors: ['sec1', 'sec2', 'sec3', 'sec4', 'sec5'],
+        menu:'#menu',
+        navigation: {
+
+        }
+     });
+  
+
+ });
